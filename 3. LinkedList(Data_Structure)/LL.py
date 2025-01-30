7e7c8e4a98fae62a3a1a531d8445345ea5fe0826
sabujrana <sabujmagarsm@gmail.com>
2025-01-30 03:54:46 -0500

    add get and set method in single linked list

--- 3. LinkedList(Data_Structure)/LL.py
+++ 3. LinkedList(Data_Structure)/LL.py
@@ -408,8 +408,8 @@ class LinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            self.tail = new_node
             self.tail.next = new_node
+            self.tail = new_node
 
         self.length += 1
         return True
@@ -452,11 +452,207 @@ class LinkedList:
         return temp
     
 
-my_linked_list = LinkedList(2)
+my_linked_list = LinkedList(1)
 my_linked_list.append(3)
+my_linked_list.append(4)
 my_linked_list.pop_first()
 my_linked_list.print_list()
 
 
+### get method code 
 
+"""
+def get(self, index):
+    if index < 0 or index >= self.length:
+        return None
+
+    temp = self.head
+    for _ in range(index):
+        temp = temp.next
+    return temp
+"""
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
         
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head 
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head #*
+            self.head = new_node
+        self.length += 1
+        return True
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        
+        return temp
+        #for value to print
+        # return temp.value
+    
+my_linked_list = LinkedList(1)
+my_linked_list.append(3)
+my_linked_list.append(4)
+print(my_linked_list.get(2))
+
+### Set value code
+"""
+def set_value(self, index, value):
+    temp = self.head
+    for i in range(index):
+        temp = temp.next
+        return True
+    return False
+    
+
+"""
+class Node:
+    
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next #infinite loop will run if we do not update temp
+        return True
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next 
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node 
+        return True
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+    
+    def set(self, index, value):
+        temp = self.get(index)
+        while temp:
+            temp.value = value
+            return True
+        return False
+my_linked_list = LinkedList(11)
+my_linked_list.append(3)
+my_linked_list.append(23)
+my_linked_list.append(20)
+my_linked_list.append(30)
+my_linked_list.set(2, 32)
+print("Linked List after using set method")
+my_linked_list.print_list()
